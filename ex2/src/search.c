77ecc8db221178ba5be31dad750ef6e76e30be86
gabtoschi <gabriel.toschi@hotmail.com>
2020-10-18 10:00:03 -0300

    Exercício 3 - Análise formal de buscas

--- ex2/src/search.c
+++ ex2/src/search.c
@@ -4,57 +4,57 @@
 
 #define BINARY_MEDIUM_INDEX(left, right) (left + right) / 2
 
-int sequentialSearch(int* data, int amount, int searchQuery) {
-  for (int index = 0; index < amount; index++) {
-    if (data[index] == searchQuery) {
-      return index;
+int sequentialSearch(int* data, int size, int searchQuery) {  // F
+  for (int index = 0; index < size; index++) {                // A+C [1a it], 2A+C [2a it+]
+    if (data[index] == searchQuery) {                         // M+C
+      return index;                                           // F
     }
   }
 
   return NOT_FOUND;
 }
 
-int iterativeBinarySearch(int* data, int amount, int searchQuery) {
-  int leftLimit = 0;
-  int rightLimit = amount - 1;
+int iterativeBinarySearch(int* data, int size, int searchQuery) { // F
+  int left = 0;                                                   // A
+  int right = size - 1;                                           // 2A
 
-  while (leftLimit < rightLimit) {
-    int checkingIndex = BINARY_MEDIUM_INDEX(leftLimit, rightLimit);
+  while (left < right) {                                          // C
+    int checkingIndex = BINARY_MEDIUM_INDEX(left, right);         // 3A
 
-    if (searchQuery == data[checkingIndex]) {
-      return checkingIndex;
+    if (searchQuery == data[checkingIndex]) {                     // M+C
+      return checkingIndex;                                       // F
     }
 
-    if (searchQuery < data[checkingIndex]) {
-      rightLimit = checkingIndex - 1;
+    if (searchQuery < data[checkingIndex]) {                      // M+C
+      right = checkingIndex - 1;                                  // 2A
     } else { // data[checkingIndex] > searchQuery
-      leftLimit = checkingIndex + 1;
+      left = checkingIndex + 1;                                   // 2A
     }
   }
 
-  return NOT_FOUND;
+  return NOT_FOUND;                                               // F
 }
 
-int recursiveBinarySearchLoop(int *data, int leftLimit, int rightLimit, int searchQuery) {
-  int checkingIndex = BINARY_MEDIUM_INDEX(leftLimit, rightLimit);
+int recursiveBinarySearchLoop(int *data, int left, int right, int searchQuery) {      // F
+  int checkingIndex = BINARY_MEDIUM_INDEX(left, right);                               // 3A
 
-  if (rightLimit >= leftLimit) {
-    if (data[checkingIndex] == searchQuery) {
-      return checkingIndex;
+  if (right >= left) {                                                                // C
+    if (data[checkingIndex] == searchQuery) {                                         // M+C
+      return checkingIndex;                                                           // F
     }
 
-    if (searchQuery < data[checkingIndex]) {
-      return recursiveBinarySearchLoop(data, leftLimit, checkingIndex - 1, searchQuery);
+    if (searchQuery < data[checkingIndex]) {                                          // M+C
+      return recursiveBinarySearchLoop(data, left, checkingIndex - 1, searchQuery);   // F+5A
     } else { // data[checkingIndex] > searchQuery
-      return recursiveBinarySearchLoop(data, checkingIndex + 1, rightLimit, searchQuery);
+      return recursiveBinarySearchLoop(data, checkingIndex + 1, right, searchQuery);  // F+5A
     }
   }
 
-  return NOT_FOUND;
+  return NOT_FOUND;                                                                   // F
 }
 
-int recursiveBinarySearch(int *data, int amount, int searchQuery) {
-  return recursiveBinarySearchLoop(data, 0, amount - 1, searchQuery);
+int recursiveBinarySearch(int *data, int size, int searchQuery) {
+  return recursiveBinarySearchLoop(data, 0, size - 1, searchQuery);
 }
 
 
